dbe4696f88dcf6d623037b383bfc8326ca32f2b4
Barrett Kowalsky <barrettkowalsky@gmail.com>
2026-01-02 13:02:54 -0700

    construction tape

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Experience from './components/Experience'
 import Footer from './components/Footer'
 import './App.css'
 
@@ -8,9 +9,11 @@ function App() {
   return (
     <>
       <div>
-        <h1>Barrett Kowalsky</h1>
-        <p>a guy doing many different things. based in denver, co.</p>
+        <h1>barrett's www</h1>
+        <p>barrett kowalsky – a guy doing many different things. based in denver, co.</p>
+        <p>🚧 this site is under active construction, partner 🤠. please wear a hardhat while navigating. 👷‍♂️</p>
         <Navbar/>
+        <Experience/>
         <Footer/>
       </div>
     </>
---
+++ src/components/Experience.tsx
@@ -0,0 +1,13 @@
+export default function Experience() {
+    return (
+        <div>
+            <p>this is career stuff for prospective employers:</p>
+            <ul>
+                <li>currently working as a devrel eng @ <a target="_blank" href="https://www.dimo.org/">dimo</a></li>
+                <li>prev. did some freelance fullstack @ <a target="_blank" href="https://www.sirch.ai/">sirch</a></li>
+                <li>prev. was a sr. project manager @ <a target="_blank" href="http://avexdesigns.com/">avex designs</a></li>
+                <li>prev. was a project manager @ <a target="_blank" href="https://www.ampagency.com/">amp agency</a></li>
+            </ul>
+        </div>
+    )
+}
