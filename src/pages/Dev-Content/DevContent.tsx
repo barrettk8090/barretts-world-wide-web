c582e6132bb64cd6713c7957fd845036a57975ac
Barrett <barrettkowalsky@gmail.com>
2026-01-23 15:39:10 -0700

    dev content

--- src/pages/Dev-Content/DevContent.tsx
+++ src/pages/Dev-Content/DevContent.tsx
@@ -2,8 +2,17 @@ export default function DevContent(){
 
     return(
         <div>
+            <h2>some samples of dev content i've written:</h2>
             <ul>
-                <li>dev content 1</li>
+                <li><a target="_blank" href="https://www.dimo.org/">dimo developer docs</a>, primary contributor</li>
+                <li><a target="_blank" href="https://dimo.co/blogs/the-pit-stop/build-on-dimo-april-2025">build on dimo: april 2025</a>, author</li>
+                <li><a target="_blank" href="https://dimo.co/blogs/the-pit-stop/build-on-dimo-may-2025">build on dimo: may 2025</a>, author</li>
+                <li><a target="_blank" href="https://dimo.co/blogs/the-pit-stop/build-on-dimo-june-2025">build on dimo: june 2025</a>, author</li>
+                <li><a target="_blank" href="https://dimo.co/blogs/the-pit-stop/introducing-dimo-webhooks">introducing dimo webhooks</a>, author</li>
+                <li><a target="_blank" href="https://www.youtube.com/watch?v=nktm5m9LhIU">youtube: dimo university: intro </a>, script & video</li>
+                <li><a target="_blank" href="https://www.youtube.com/watch?v=bfTwfAHJdKI">youtube: dimo university: dev licenses</a>, script & video</li>
+                <li><a target="_blank" href="https://barrettk.hashnode.dev/recreating-a-simple-goodreads-book-tracker-using-python-sqlalchemy-and-inquirer">recreating a simple goodreads book tracker using python</a>, author</li>
+                <li><a target="_blank" href="https://barrettk.hashnode.dev/creating-your-first-full-stack-dapp-with-solidity-hardhat-and-react">creating your first full-stack dApp</a>, author</li>
             </ul>
         </div>
     )
